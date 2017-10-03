a1971d3a90236cc107747e05ac359982182c5635
Christian Wengert <christian.wengert@smartpowerpool.ch>
2017-10-03 12:16:23 +0200

    alles docx

--- app.py
+++ app.py
@@ -161,10 +161,10 @@ class Root:
         print(currdir)
         self.documents = [os.path.join(currdir, "documents", "Gruendungsurkunde.docx"),
                           os.path.join(currdir, "documents", "Statuten.docx"),
-                          os.path.join(currdir, "documents", "Wahlannahmeerklaerung.doc"),
+                          os.path.join(currdir, "documents", "Wahlannahmeerklaerung.docx"),
                           os.path.join(currdir, "documents", "Lex-Friedrich.docx"),
                           os.path.join(currdir, "documents", "Stampa.docx"),
-                          os.path.join(currdir, "documents", "ZKB_Brief.doc")
+                          os.path.join(currdir, "documents", "ZKB_Brief.docx")
                           ]
 
     @cherrypy.expose
